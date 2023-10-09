9ef102358a1d4ef19b5cc4990601a0e49336c0cd
Tyuiu.FominVN@outlook.com <hialdez111@gmail.com>
2023-10-10 00:54:02 +0500

    Add project files.

---
+++ Tyuiu.FominIN.Sprint1.Task0.V4.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.FominIN.Sprint1.Task0.V4.Lib;
+
+namespace Tyuiu.FominIN.Sprint1.Task0.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(6, res);
+        }
+    }
+}
---
+++ Tyuiu.FominIN.Sprint1.Task0.V4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.FominIN.Sprint1.Task0.V4.Lib;
+
+namespace Tyuiu.FominIN.Sprint1.Task0.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Фомин И. Н. | ПКТб-23-1";
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* Спринт #1                                                             *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                      *");
+            Console.WriteLine("* Задание #0                                                            *");
+            Console.WriteLine("* Вариант #4                                                            *");
+            Console.WriteLine("* Выполнил: Фомин Иван Николаевич | ПКТб-23-1                           *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                              *");
+            Console.WriteLine("* Написать программу, котрая вычисляет выражение 4/2*5/(3+2)*(5-2)      *");
+            Console.WriteLine("* и печатает результат на экране.                                       *");
+            Console.WriteLine("*                                                                       *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                      *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* 4/2*5/(3+2)*(5-2)                                                     *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                            *");
+            Console.WriteLine("*************************************************************************");
+
+            Console.WriteLine(ds.Calculate());
+
+            Console.ReadLine();
+        }
+    }
+}
