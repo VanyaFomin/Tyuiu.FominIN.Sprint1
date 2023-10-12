97d89612bafacb56d15f65a8e48a75b7bc3dc07d
Tyuiu.FominVN@outlook.com <hialdez111@gmail.com>
2023-10-12 20:05:57 +0500

    Task4

---
+++ Tyuiu.FominIN.Sprint1.Task4.V1.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.FominIN.Sprint1.Task4.V1.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.FominIN.Sprint1.Task4.V1.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("93a9f8c3-3638-43b4-b736-a0c2ccfe5a07")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.FominIN.Sprint1.Task4.V1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.FominIN.Sprint1.Task4.V1.Lib;
+
+namespace Tyuiu.FominIN.Sprint1.Task4.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Фомин И. Н. | ПКТб-23-1";
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* Спринт #1                                                                    *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                             *");
+            Console.WriteLine("* Задание #4                                                                   *");
+            Console.WriteLine("* Вариант #1                                                                   *");
+            Console.WriteLine("* Выполнил: Фомин Иван Николаевич | ПКТб-23-1                                  *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                     *");
+            Console.WriteLine("* Написать программу, котрая запрашивает у пользователя исходные данные,       *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.                  *");
+            Console.WriteLine("*                                                                              *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                             *");
+            Console.WriteLine("********************************************************************************");
+
+            int x;
+   
+            Console.WriteLine("Введите значение X: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                   *");
+            Console.WriteLine("********************************************************************************");
+
+            
+           Console.WriteLine("1 / (x + 2) ^ 2 = " + ds.Calculate(x).ToString("F3"));
+
+           Console.ReadKey();
+        }
+    }
+}
